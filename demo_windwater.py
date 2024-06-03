c5273338514a9c51e96195dbec5cd134fc64802e
Jorge Eduardo Velasco-Zavala <42640851+jorgeev@users.noreply.github.com>
2024-06-02 21:19:40 -0600

    updates example

--- demo_windwater.py
+++ demo_windwater.py
@@ -13,21 +13,23 @@ import cartopy.crs as ccrs
 
 from njordr.models import njordr_waterwind
 
-model = njordr_waterwind(lon0=-87, lat0=20, 
-                         particles=10000, dt=60, outputstep=3600,
+model = njordr_waterwind(lon0=-87., lat0=20, 
+                         particles=100000, dt=300, outputstep=3600,
                          start_time='2023-08-12 00:00:00',
-                         spill_duration=10,
-                         difussivity=0.1, duration=40)
+                         spill_duration=40, windage=0.02,
+                         difussivity=0.05, duration=40)
 print(model.difussivity)
 model.run()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-ax.set_extent([-99.5214,-78.5589,16.5369,32.4075])
+ax.set_extent([-87.5,-86.,19.5,21.5])
 gl = ax.gridlines(crs=ccrs.PlateCarree(), 
                   draw_labels=True, 
                   linewidth=0.6, alpha=0.8, linestyle='--')
 ax.scatter(model.lon.get(), model.lat.get(), s=0.5, c='k')
+ax.scatter(-87, 20, s=5, marker='x', c='r')
 ax.coastlines()
 ax.set_aspect(1)
+#plt.savefig('eg_01.jpg',  bbox_inches='tight', pad_inches=0.1, pil_kwargs={'quality':95})
 fig.show()
